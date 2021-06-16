b331322c52281a52217f561fb66ce1f22c26a055
Ces95 <c-diazperez95>
2021-06-16 10:44:36 -0400

    tercer commit

--- tdd_kata.py
+++ tdd_kata.py
@@ -1,11 +1,9 @@
-import unittest
 from katai import *
+import unittest
 
 
 
 class Tdd(unittest.TestCase):
-    def setUp(self):
-        pass
 
     def Test1_conversion(self):
         self.assertEqual(rgb_to_hex(42, 87, 235),'#2a57eb')
@@ -14,7 +12,12 @@ class Tdd(unittest.TestCase):
         self.assertNotEqual(rgb_to_hex(42, 87, 205),'#2a57eb')
 
     def Test3_prueba_de_valores(self):
-        self.assertRaise(ValueError,rgb_to_hex,42, 87, 205)
+        self.assertRaise(ValueError,rgb_to_hex,-42, 87, 255)
+
+    def Test4_prueba_de_cantidad_de_valore(self):
+        self.assertRaise(TypeError,rgb_to_hex,42, 87, 255, 5)
+        self.assertRaise(TypeError,rgb_to_hex,42, 87)
+        self.assertRaise(TypeError,rgb_to_hex, 5)
 
 
 
