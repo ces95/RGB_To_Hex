dc9b6d1c7822746c0ca974620480dc46fb7984cc
Ces95 <c-diazperez95>
2021-06-16 10:52:27 -0400

    final commit

--- katai.py
+++ katai.py
@@ -4,7 +4,8 @@ def rgb_to_hex(a,b,c):
     if((a>=0 and a<=255) and (b>=0 and b<=255) and (c>=0 and c<=255)):
         hex_color = '#{:02x}{:02x}{:02x}'.format(a,b,c)
 
-        print( hex_color)
+        #print( hex_color)
+        return hex_color
 
     else:
         raise ValueError(" Error")
--- tdd_kata.py
+++ tdd_kata.py
@@ -5,19 +5,22 @@ import unittest
 
 class Tdd(unittest.TestCase):
 
-    def Test1_conversion(self):
+    def setUp(self):
+        pass
+
+    def test1_conversion(self):
         self.assertEqual(rgb_to_hex(42, 87, 235),'#2a57eb')
 
-    def Test2_conversion(self):
+    def test2_conversion(self):
         self.assertNotEqual(rgb_to_hex(42, 87, 205),'#2a57eb')
 
-    def Test3_prueba_de_valores(self):
-        self.assertRaise(ValueError,rgb_to_hex,-42, 87, 255)
+    def test3_prueba_de_valores(self):
+        self.assertRaises(ValueError,rgb_to_hex,-42, 87, 255)
 
-    def Test4_prueba_de_cantidad_de_valore(self):
-        self.assertRaise(TypeError,rgb_to_hex,42, 87, 255, 5)
-        self.assertRaise(TypeError,rgb_to_hex,42, 87)
-        self.assertRaise(TypeError,rgb_to_hex, 5)
+    def test4_prueba_de_cantidad_de_valore(self):
+        self.assertRaises(TypeError,rgb_to_hex,42, 87, 255, 5)
+        self.assertRaises(TypeError,rgb_to_hex,42, 87)
+        self.assertRaises(TypeError,rgb_to_hex, 5)
 
 
 
